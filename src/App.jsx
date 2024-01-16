da5ab334ae0657b2be69d8a084cc223bfaba9967
Salunkhe1101-Neha <salunkheneha174@gmail.com>
2024-01-16 05:41:28 +0530

    Add files

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import "./styles.css";
+import Plx from "react-plx";
+
+export default function App() {
+  return (
+    <div>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 900,
+            easing: "ease-in",
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.6,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%",
+          height:'100vh',
+          zIndex: 100
+        }}
+      >
+        <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
+      </Plx>
+      
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 800,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.18,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%"
+        }}
+      >
+        <img style={{ width: "100%" }} src="back.jpg" alt="background" />
+      </Plx>
+      
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 400,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 0,
+                property: "opacity"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: "22vw",
+          width: "100%"
+        }}
+      >
+
+        <h1 style={{ color: 'white' }}>The Goonies</h1>
+      </Plx>
+
+      
+
+
+      <div
+        style={{
+          position: "fixed",
+          lefft: 0,
+          top: 0,
+          zIndex: 200,
+          paddingTop: "56%",
+          height: "400vh",
+          width: "100%"
+        }}
+      >
+        <div
+          style={{
+            background: "black",
+            height: "100%"
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
